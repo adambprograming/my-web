ce1cf9e3516d460988f196cbcedd53f338805bc3
Adam <adambprograming@yahoo.com>
2023-10-15 06:26:04 +0200

    language indicator set, smooth slide to article from menu set

--- components/buttons-settings/btn-lang.component.jsx
+++ components/buttons-settings/btn-lang.component.jsx
@@ -20,8 +20,38 @@ const LanguageBtn = () => {
   const [initLang, setInitLang] = useState()
   // Get other parts of pathname then language ('cz'/'en')
   const pathname = usePathname().split("/").slice(2);
+  // This useEffect handle first load of page and set initial language based on url and also if client have 'cs' in languages, if not effect set lang of page to en
   useEffect(() => {
+    // Set state of InitLang to Init lang of page bcs of proper btn Initial position
     setInitLang(document.documentElement.getAttribute('lang'))
+    // If there is more languages check if it includes 'cs', if does and if lang of page is 'cz', make it 'en', else dont change anything
+    if (navigator.languages != undefined){
+      if (!navigator.languages.includes('cs') && document.documentElement.getAttribute('lang') === 'cz') {
+        setInitLang('en')
+        // Set atribute of html tag, lang to en
+        document.documentElement.setAttribute("lang", "en");
+        // Set language throught context to en
+        setLanguage("en");
+        // Change URL without re-render
+        window.history.pushState('EN', undefined, '/en')
+        // redirect to new URL without re-render
+        redirect(`/en/${pathname}`, 'replace');
+
+      }
+      // If there is only one language and that's 'cs' and lang of page is 'cz', make it 'en', else dont change anything
+    } else if (navigator.language !== 'cs' && document.documentElement.getAttribute('lang') === 'cz'){
+      setInitLang('en')
+      // Set atribute of html tag, lang to en
+      document.documentElement.setAttribute("lang", "en");
+      // Set language throught context to en
+      setLanguage("en");
+      // Change URL without re-render
+      window.history.pushState('EN', undefined, '/en')
+      // redirect to new URL without re-render
+      redirect(`/en/${pathname}`, 'replace');
+              setInitLang(document.documentElement.getAttribute('lang'))
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   // Create btn handler
   const switchLang = () => {
--- components/menu-dropdown/menu-dropdown.component.jsx
+++ components/menu-dropdown/menu-dropdown.component.jsx
@@ -7,28 +7,27 @@ import { useContext, useState } from 'react';
 import { LanguageContext } from "../../context/lang.context";
 
 const MenuDropdown = () => {
+
   const [activeMenu, setActiveMenu] = useState(false);
     const { languageDict } = useContext(LanguageContext)
     const handleMenuClick = () => {
-        setActiveMenu(!activeMenu);
+      document.getElementsByClassName('menu-icon').item(0).classList.toggle('active');
+      document.getElementsByClassName('line-1').item(0).classList.remove('no-animation')
+      document.getElementsByClassName('line-2').item(0).classList.remove('no-animation')
+      document.getElementsByClassName('line-3').item(0).classList.remove('no-animation')
+      setActiveMenu(!activeMenu);
     };
     return (
     <nav className='nav'>
-      <div className="menu-icon" onClick={function() {
-        document.getElementsByClassName('menu-icon').item(0).classList.toggle('active');
-        document.getElementsByClassName('line-1').item(0).classList.remove('no-animation')
-        document.getElementsByClassName('line-2').item(0).classList.remove('no-animation')
-        document.getElementsByClassName('line-3').item(0).classList.remove('no-animation')
-        handleMenuClick()
-      }}>
+      <div className="menu-icon" onClick={() => {handleMenuClick()}}>
         <div className="line-1 no-animation"></div>
         <div className="line-2 no-animation"></div>
         <div className="line-3 no-animation"></div>
       </div>
       <ul className={`dropdown ${activeMenu === true ? "active" : ""}`}>
-        <li className='dropdown-item'><a href="#article-education"><h5>{languageDict.article_header.menu.education}</h5></a></li>
-        <li className='dropdown-item'><a href="#article-techstack"><h5>{languageDict.article_header.menu.techstack}</h5></a></li>
-        <li className='dropdown-item'><a href="#article-contact"><h5>{languageDict.article_header.menu.contact}</h5></a></li>
+        <li className='dropdown-item'><button onClick={() => {document.getElementById('article-education').scrollIntoView({ behavior: 'smooth' })}}><h5>{languageDict.article_header.menu.education}</h5></button></li>
+        <li className='dropdown-item'><button onClick={() => {document.getElementById('article-techstack').scrollIntoView({ behavior: 'smooth' })}}><h5>{languageDict.article_header.menu.techstack}</h5></button></li>
+        <li className='dropdown-item'><button onClick={() => {document.getElementById('article-contact').scrollIntoView({ behavior: 'smooth' })}}><h5>{languageDict.article_header.menu.contact}</h5></button></li>
       </ul>
     </nav>
   )
