00fbd092f0feae7466ae18600300e3b35ba9f996
Adam <adambprograming@yahoo.com>
2023-10-17 21:59:30 +0200

    Test of lazy loading, added aria-label to links

--- app/[lang]/page.jsx
+++ app/[lang]/page.jsx
@@ -1,8 +1,15 @@
+// Next functions
+import dynamic from "next/dynamic";
 // Components
-import ArticleHero from "../../components/article-hero/article-hero.component";
-import ArticleEducation from "../../components/article-education/article-education.component";
-import ArticleTechstack from "../../components/article-techstack/article-techstack.component";
-import ArticleContact from "../../components/article-contact/article-contact.component";
+import LoadingBlock from "../../components/r-loading-block/loading-block.component";
+const ArticleHero = dynamic(() => import('../../components/article-hero/article-hero.component'), { ssr: false, loading: () => <LoadingBlock width={'100%'} height={'300px'} />})
+const ArticleEducation = dynamic(() => import('../../components/article-education/article-education.component'), { ssr: false, loading: () => <LoadingBlock width={'100%'} height={'300px'} />})
+const ArticleTechstack = dynamic(() => import('../../components/article-techstack/article-techstack.component'), { ssr: false, loading: () => <LoadingBlock width={'100%'} height={'300px'} />})
+const ArticleContact = dynamic(() => import('../../components/article-contact/article-contact.component'), { ssr: false, loading: () => <LoadingBlock width={'100%'} height={'300px'} />})
+// import ArticleHero from "../../components/article-hero/article-hero.component";
+// import ArticleEducation from "../../components/article-education/article-education.component";
+// import ArticleTechstack from "../../components/article-techstack/article-techstack.component";
+// import ArticleContact from "@/components/article-contact/article-contact.component";
 
 export default function Home() {
   return (
